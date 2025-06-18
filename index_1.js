47043ae31bc4b4c2122b1d0781e6e926863d98a7
PraveenWE <praveentecsl@gmail.com>
2025-06-18 16:38:00 +0530

    create get and post req

--- index_1.js
+++ index_1.js
@@ -1,6 +1,8 @@
 import express from 'express'
 
+
 const app= express();
+app.use(express.json())
 
 app.listen(4001,()=>{
     console.log("server is running  praveen");
@@ -8,4 +10,13 @@ app.listen(4001,()=>{
 
 app.get('/praveen',(req,res)=>{
     res.send('your get request is working ');
+})
+
+app.get('/chandu',(re,ro)=>{
+    ro.send('oh man your get is okey');
+})
+
+app.post('/submit',(req,res)=>{
+    console.log('you pass the data as : ',req.body);
+    res.send('I got your data');
 })
